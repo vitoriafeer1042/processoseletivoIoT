9531f08ff5564d7b03501e6b57b3304551931c95
vitoriafeer1042 <vitoriafeer1042@gmail.com>
2026-07-26 23:28:27 -0300

    fix(light): corrige logica do sensor LDR usando valores brutos do ADC
    Remove funcao get_lux() que tinha formula do divisor de tensao invertida.
    O modulo LDR do Wokwi usa circuito VCC->10K->AO->LDR->GND, onde
    ADC alto = escuro e ADC baixo = claro. A formula anterior assumia
    o circuito oposto, invertendo toda a deteccao.
    
    Agora usa limiares diretos do ADC com histerese (2000/1000).

--- src/main.py
+++ src/main.py
@@ -1,70 +1,68 @@
 import machine
 import time
-import math
+
+# Configuracao de pinos
+pino_sensor = machine.ADC(machine.Pin(34))
+pino_sensor.atten(machine.ADC.ATTN_11DB)
+pino_sensor.width(machine.ADC.WIDTH_12BIT)
+
+pino_botao = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# Limiares de deteccao (valores brutos do ADC)
+# No modulo LDR do Wokwi: escuro = ADC alto, claro = ADC baixo
+ADC_ESCURO = 2000    # Acima disso: objeto bloqueando o sensor
+ADC_CLARO = 1000     # Abaixo disso: caminho livre
+
+# Temporizadores
+LIMITE_MICRO_PARADA = 5000  # ms sem movimento = micro-parada
+DEBOUNCE_MS = 50
+
+# Estado do sistema
+pecas_contadas = 0
+obstruido = False
+inicio_obstrucao = 0
+alerta_emitido = False
+
+# Estado do botao
+estado_botao_ant = 1
+tempo_botao_ant = 0
 
 print("Contador de Producao Inicializado")
 
-# Setup pins
-ldr_pin = machine.ADC(machine.Pin(34))
-ldr_pin.atten(machine.ADC.ATTN_11DB) # Full range: 0-3.3v
-
-btn1 = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
-
-# Global variables
-total_pieces = 0
-is_blocked = False
-block_start_time = 0
-micro_stop_alerted = False
-last_btn_state = 1
-last_btn_time = 0
-
-def get_lux(adc_val):
-    if adc_val <= 0:
-        return 0
-    if adc_val >= 4095:
-        return 100000
-    try:
-        r_ldr = 10000 * (4095 / adc_val - 1)
-        if r_ldr <= 0:
-            return 100000
-        lux = 10 * math.pow(50000 / r_ldr, 1 / 0.7)
-        return lux
-    except:
-        return 0
-
 while True:
-    current_time = time.ticks_ms()
-    
-    # 1. Handle LDR reading
-    adc_val = ldr_pin.read()
-    lux = get_lux(adc_val)
-    
-    # 2. Logic for detection
-    if not is_blocked and lux < 100:
-        is_blocked = True
-        block_start_time = current_time
-        micro_stop_alerted = False
-    elif is_blocked and lux > 500:
-        is_blocked = False
-        total_pieces += 1
-        print("Peca detectada! Total: {}".format(total_pieces))
-        
-    # 3. Logic for Micro-stop (lux < 100 for > 5 seconds)
-    if is_blocked and not micro_stop_alerted:
-        if time.ticks_diff(current_time, block_start_time) > 5000:
+    agora = time.ticks_ms()
+
+    # Leitura do sensor de luminosidade (valor bruto do ADC)
+    leitura_adc = pino_sensor.read()
+
+    # Deteccao de pecas por transicao de luminosidade
+    if not obstruido and leitura_adc > ADC_ESCURO:
+        # Transicao claro -> escuro: objeto entrou na frente do sensor
+        obstruido = True
+        inicio_obstrucao = agora
+        alerta_emitido = False
+    elif obstruido and leitura_adc < ADC_CLARO:
+        # Transicao escuro -> claro: objeto passou completamente
+        obstruido = False
+        pecas_contadas += 1
+        print("Peca detectada! Total: {}".format(pecas_contadas))
+
+    # Verificacao de micro-parada (sensor bloqueado por tempo excessivo)
+    if obstruido and not alerta_emitido:
+        if time.ticks_diff(agora, inicio_obstrucao) > LIMITE_MICRO_PARADA:
             print("Alerta: Micro-parada detectada!")
-            micro_stop_alerted = True
-            
-    # 4. Handle Button for Reset
-    btn_state = btn1.value()
-    # Debounce
-    if btn_state != last_btn_state and time.ticks_diff(current_time, last_btn_time) > 50:
-        if btn_state == 0: # Pressed
-            total_pieces = 0
-            is_blocked = False
-            micro_stop_alerted = False
-            print("Turno resetado com sucesso. Contadores zerados.")
-        last_btn_state = btn_state
-        last_btn_time = current_time
-            
-    time.sleep_ms(50) # Non-blocking delay
+            alerta_emitido = True
+
+    # Leitura do botao de reset com debounce
+    estado_botao = pino_botao.value()
+    if estado_botao != estado_botao_ant:
+        if time.ticks_diff(agora, tempo_botao_ant) > DEBOUNCE_MS:
+            if estado_botao == 0:
+                pecas_contadas = 0
+                obstruido = False
+                alerta_emitido = False
+                print("Turno resetado com sucesso. Contadores zerados.")
+            estado_botao_ant = estado_botao
+            tempo_botao_ant = agora
+
+    time.sleep_ms(50)
